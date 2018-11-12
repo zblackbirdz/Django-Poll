c6159b2971530723c90c39cd7dbc44fbe4b4d5fe
Richard <rwcivil@gmail.com>
2018-11-12 09:51:06 +0000

    Tutorial - 02

---
+++ batch files/create-01-project.bat
@@ -0,0 +1,5 @@
+@rem create the project files (Note the ".")
+django-admin startproject mysite . 
+
+@rem launch the site (to check that it is running)
+python manage.py runserver
---
+++ batch files/create-02-polls.bat
@@ -0,0 +1,2 @@
+@rem create the files for the app Polls (Note the ".")
+python manage.py startapp polls
